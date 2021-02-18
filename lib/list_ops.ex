98443087c88284a80689fe9a013b9df0b9cbef27
Morgan Donze <donzemorgan@gmail.com>
2021-02-18 10:35:46 -0500

    completed

--- lib/list_ops.ex
+++ lib/list_ops.ex
@@ -7,31 +7,80 @@ defmodule ListOps do
 
   @spec count(list) :: non_neg_integer
   def count(l) do
-
+    ListOps.reduce(l, 0, fn (_,acc) -> acc + 1 end )
   end
 
   @spec reverse(list) :: list
   def reverse(l) do
+    reverse(l, [])
+  end
+
+  def reverse([], reversed) do
+    reversed
+  end
+
+  def reverse([h | t], reversed) do
+    reverse(t, [h | reversed])
   end
 
   @spec map(list, (any -> any)) :: list
   def map(l, f) do
+    reverse( map(l, [], f) )
+  end
+
+  def map([], mapped, _) do
+    mapped
+  end
+
+  def map([h | t], mapped, f) do
+    map(t, [f.(h) | mapped], f)
   end
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
+  def filter([], f), do: []
+
+  def filter([h | t], f) do
+    if f.(h), do: [h | filter(t, f)], else: filter(t, f)
   end
 
   @type acc :: any
   @spec reduce(list, acc, (any, acc -> acc)) :: acc
-  def reduce(l, acc, f) do
+  def reduce([], acc, f) do
+    acc
+  end
+
+  def reduce([h | []], acc, f) do
+    f.(h, acc)
+  end
+
+  def reduce([h | t], acc, f) do
+    reduce(t, f.(h, acc), f)
   end
 
   @spec append(list, list) :: list
-  def append(a, b) do
+
+  def append([], []) do
+    []
+  end
+
+  def append(a, []) do
+    a
+  end
+
+  def append([], b) do
+    b
+  end
+
+  def append([h | t], b) do
+    [h | append(t, b)]
   end
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
+  def concat([]) do 
+    []
+  end
+
+  def concat([h |t]) do
+    append(h, concat(t))
   end
 end
